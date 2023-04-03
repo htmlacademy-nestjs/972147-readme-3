bdca1a9af60852ebe3b6f0c320c2d412c6bec317
Владислав  Клева <vladislavkleva@Vladislavs-MacBook-Pro.local>
2023-04-04 02:59:36 +0700

    Finish spec

--- project/apps/users/src/app/auth/auth.controller.ts
+++ project/apps/users/src/app/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from "./auth.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { LoginUserDto } from "./dto/login-user.dto";
@@ -14,15 +14,33 @@ export class AuthController {
   ) {
   }
 
-  @ApiResponse({ type: LoggedUserRdo })
+  @ApiResponse({
+    type: LoggedUserRdo,
+    status: HttpStatus.OK,
+    description: 'User has been successfully logged.'
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Email or password is incorrect'
+  })
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   public async login(@Body() dto: LoginUserDto): Promise<LoggedUserRdo> {
     const user = await this.authService.validateUser(dto);
     return fillObject(LoggedUserRdo, user);
   }
 
-  @ApiResponse({ type: LoggedUserRdo })
+  @ApiResponse({
+    type: LoggedUserRdo,
+    status: HttpStatus.CREATED,
+    description: 'User has been successfully registered.'
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'User with this email already exists'
+  })
   @Post('register')
+  @HttpCode(HttpStatus.CREATED)
   public async create(@Body() dto: CreateUserDto): Promise<LoggedUserRdo> {
     const newUser = await this.authService.registerUser(dto)
     return fillObject(LoggedUserRdo, newUser);
--- project/apps/users/src/app/blog-user/blog-user.controller.ts
+++ project/apps/users/src/app/blog-user/blog-user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
 import { BlogUserService } from "./blog-user.service";
 import { fillObject } from "@project/util/util-core";
 import { UserRdo } from "./rdo/user.rdo";
@@ -14,22 +14,52 @@ export class BlogUserController {
   ) {
   }
 
-  @ApiResponse({ type: UserRdo })
+  @ApiResponse({
+    type: UserRdo,
+    status: HttpStatus.OK,
+    description: 'User has been successfully retrieved.'
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'User not found'
+  })
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
   public async getUserById(@Param('id') id: string): Promise<UserRdo> {
     const user = await this.blogUserService.getUser(id);
     return fillObject(UserRdo, user);
   }
 
-  @ApiResponse({ type: UserRdo })
+  @ApiResponse({
+    type: UserRdo,
+    status: HttpStatus.OK,
+    description: 'User has been successfully updated'
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'User not found'
+  })
   @Post(':id/update')
+  @HttpCode(HttpStatus.OK)
   public async update(@Body() dto: UpdateUserDto, @Param('id') id: string): Promise<UserRdo> {
     const user = await this.blogUserService.updateUser(id, dto);
     return fillObject(UserRdo, user);
   }
 
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Password has been successfully updated'
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'Old password is incorrect'
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'User not found'
+  })
   @Post(':id/change-password')
-  @ApiResponse({ type: undefined })
+  @HttpCode(HttpStatus.OK)
   public async changePassword(@Body() dto: UpdatePasswordDto, @Param('id') id: string) {
     await this.blogUserService.updatePassword(id, dto);
     return undefined;
