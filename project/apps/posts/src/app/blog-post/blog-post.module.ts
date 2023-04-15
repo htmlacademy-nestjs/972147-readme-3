841be1ebec764c7b28fe0af2d0f208a211e31fc3
Владислав  Клева <vladislavkleva@Vladislavs-MacBook-Pro.local>
2023-04-16 00:28:10 +0700

    Removes blog-post memory repository

--- project/apps/posts/src/app/blog-post/blog-post.module.ts
+++ project/apps/posts/src/app/blog-post/blog-post.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlogPostService } from './blog-post.service';
 import { BlogPostController } from './blog-post.controller';
-import { BlogPostMemoryRepository } from "./repositories/blog-post.memory.repository";
 import { BlogPostDbRepository } from "./repositories/blog-post.db.repository";
 
 @Module({
-  providers: [BlogPostService, BlogPostMemoryRepository, BlogPostDbRepository],
+  providers: [BlogPostService, BlogPostDbRepository],
   controllers: [BlogPostController],
 })
 export class BlogPostModule {}
